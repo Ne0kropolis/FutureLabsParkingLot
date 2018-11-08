27bfb44b6d0f57773693c9477fae66f8fe41fc9d
Fortune Dederen <dedfor01@CSGICORP.COM>
2018-11-08 14:02:35 +0200

    Initial Project Setup: Added FutureGadgetParkingLot Project
    Reviewd By: N/A

---
+++ src/main/java/com/FutureGadgetParkingLot/app/HelloApplication.java
@@ -0,0 +1,19 @@
+package com.FutureGadgetParkingLot.app;
+
+import com.FutureGadgetParkingLot.rest.HelloRestService;
+
+import javax.ws.rs.core.Application;
+import java.util.HashSet;
+import java.util.Set;
+
+public class HelloApplication extends Application {
+    private Set<Object> singletons = new HashSet<Object>();
+    public HelloApplication() {
+        singletons.add(new HelloRestService());
+    }
+
+    @Override
+    public Set<Object> getSingletons() {
+        return singletons;
+    }
+}
